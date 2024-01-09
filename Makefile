96f38ea126490ec2b42fabc37cb0d110afc9f641
artiehumphreys <artieh2003@gmail.com>
2024-01-09 17:46:30 -0500

    Working on linker now. Fixed issues with making kernel

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-GPPPARAMS = -m32 -ffreestanding -O2 -Wall -Wextra
+GPPPARAMS = -m32 -ffreestanding -O2 -Wall -Wextra -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-leading-underscore
 ASPARAMS = --32
 LDPARAMS = -m elf_i386 -T
 objects = loader.o kernel.o
--- kernel.cpp
+++ kernel.cpp
@@ -1,3 +1,12 @@
+void printf(char *str)
+{
+    unsigned short *videoMemory = (unsigned short *)0xb8000;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        videoMemory[i] = (unsigned short)(videoMemory[i] & 0xFF00) | str[i];
+    }
+}
+
 void kernelMain(void *multibootStructure, unsigned int magicNumber)
 {
     while (1)
